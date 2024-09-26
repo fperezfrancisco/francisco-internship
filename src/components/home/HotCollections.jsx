3220768832dc812a8a6394cf0ffc2d43bbfc43de
Francisco Perez <fperezfrancisco4@gmail.com>
2024-09-26 11:48:09 -0700

    added carousel functionality using owl carousel

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,23 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { Link, useSearchParams } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import { Link } from "react-router-dom";
 import axios from "axios";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
 const HotCollections = () => {
   //hot collections list state
   const [hotCollectionsList, setHotCollectionsList] = useState();
 
   //fetching hot collections data using axios
-  const getHotCollectionsData = () => {
+  async function getHotCollectionsData() {
     axios
       .get(
         "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
       )
       .then((resp) => {
+        console.log(resp.data);
         setHotCollectionsList(resp.data);
       })
       .catch((err) => console.log(err));
+  }
+
+  //owl carousel
+  const options = {
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      650: {
+        items: 2,
+      },
+      768: { items: 3 },
+      1024: { items: 4 },
+    },
   };
 
   useEffect(() => {
@@ -34,41 +54,44 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {hotCollectionsList &&
-            hotCollectionsList.map((element) => (
-              <div
-                className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
-                key={element.id}
-              >
-                <div className="nft_coll">
-                  <div className="nft_wrap">
-                    <Link to="/item-details">
-                      <img
-                        src={element.nftImage}
-                        className="lazy img-fluid"
-                        alt=""
-                      />
-                    </Link>
-                  </div>
-                  <div className="nft_coll_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-coll"
-                        src={element.authorImage}
-                        alt=""
-                      />
-                    </Link>
-                    <i className="fa fa-check"></i>
-                  </div>
-                  <div className="nft_coll_info">
-                    <Link to="/explore">
-                      <h4>{element.title}</h4>
-                    </Link>
-                    <span>ERC-{element.code}</span>
+          {hotCollectionsList && (
+            <OwlCarousel className="owl-theme" {...options}>
+              {hotCollectionsList.map((element) => (
+                <div
+                  className="col-lg-3 col-md-6 col-sm-6 col-xs-12 hotCollectionContainer"
+                  key={element.id}
+                >
+                  <div className="nft_coll">
+                    <div className="nft_wrap">
+                      <Link to="/item-details">
+                        <img
+                          src={element.nftImage}
+                          className="lazy img-fluid"
+                          alt=""
+                        />
+                      </Link>
+                    </div>
+                    <div className="nft_coll_pp">
+                      <Link to="/author">
+                        <img
+                          className="lazy pp-coll"
+                          src={element.authorImage}
+                          alt=""
+                        />
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info">
+                      <Link to="/explore">
+                        <h4>{element.title}</h4>
+                      </Link>
+                      <span>ERC-{element.code}</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </OwlCarousel>
+          )}
         </div>
       </div>
     </section>
