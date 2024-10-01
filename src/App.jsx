51fb6b28d69e939665eb63c27ef4bff1995d1614
Francisco Perez <fperezfrancisco4@gmail.com>
2024-10-01 12:10:43 -0700

    spelled github repo wrong

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Router basename="/francisco-internship">
+    <Router basename="/franciso-internship">
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
