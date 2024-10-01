070b398b133544523cf1aaebd5394cd1df75650a
Francisco Perez <fperezfrancisco4@gmail.com>
2024-10-01 11:42:00 -0700

    added all animations

--- src/components/home/BrowseByCategory.jsx
+++ src/components/home/BrowseByCategory.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 
 const BrowseByCategory = () => {
   return (
@@ -12,37 +16,78 @@ const BrowseByCategory = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-left"
+            data-aos-duration="600"
+            data-aos-easing="ease"
+            data-aos-once="true"
+          >
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-image"></i>
               <span>Art</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-left"
+            data-aos-duration="600"
+            data-aos-delay="300"
+            data-aos-easing="ease"
+            data-aos-once="true"
+          >
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-music"></i>
               <span>Music</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-left"
+            data-aos-duration="600"
+            data-aos-delay="500"
+            data-aos-easing="ease"
+            data-aos-once="true"
+          >
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-search"></i>
               <span>Domain Names</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-left"
+            data-aos-duration="600"
+            data-aos-delay="650"
+            data-aos-easing="ease"
+            data-aos-once="true"
+          >
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-globe"></i>
               <span>Virtual Worlds</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-left"
+            data-aos-duration="600"
+            data-aos-delay="750"
+            data-aos-easing="ease"
+            data-aos-once="true"
+          >
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-vcard"></i>
               <span>Trading Cards</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-left"
+            data-aos-duration="600"
+            data-aos-delay="800"
+            data-aos-easing="ease"
+            data-aos-once="true"
+          >
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-th"></i>
               <span>Collectibles</span>
--- src/components/home/LandingIntro.jsx
+++ src/components/home/LandingIntro.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 
 const LandingIntro = () => {
   return (
@@ -7,10 +11,25 @@ const LandingIntro = () => {
         <div className="row">
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_wallet"></i>
+              <i
+                className="bg-color-2 i-boxed icon_wallet"
+                data-aos="fade-up"
+                data-aos-offset="50"
+                data-aos-delay="0"
+                data-aos-duration="1200"
+                data-aos-easing="ease"
+                data-aos-once="true"
+              ></i>
               <div className="text">
                 <h4 className="">Set up your wallet</h4>
-                <p>
+                <p
+                  data-aos="fade-up"
+                  data-aos-offset="50"
+                  data-aos-delay="150"
+                  data-aos-duration="1500"
+                  data-aos-easing="ease"
+                  data-aos-once="true"
+                >
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
@@ -20,10 +39,25 @@ const LandingIntro = () => {
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_cloud-upload_alt"></i>
+              <i
+                className="bg-color-2 i-boxed icon_cloud-upload_alt"
+                data-aos="fade-up"
+                data-aos-offset="50"
+                data-aos-delay="0"
+                data-aos-duration="1200"
+                data-aos-easing="ease"
+                data-aos-once="true"
+              ></i>
               <div className="text">
                 <h4 className="">Add your NFT's</h4>
-                <p>
+                <p
+                  data-aos="fade-up"
+                  data-aos-offset="50"
+                  data-aos-delay="150"
+                  data-aos-duration="1500"
+                  data-aos-easing="ease"
+                  data-aos-once="true"
+                >
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
@@ -33,10 +67,25 @@ const LandingIntro = () => {
           </div>
           <div className="col-lg-4 col-md-6 mb-sm-30">
             <div className="feature-box f-boxed style-3">
-              <i className="bg-color-2 i-boxed icon_tags_alt"></i>
+              <i
+                className="bg-color-2 i-boxed icon_tags_alt"
+                data-aos="fade-up"
+                data-aos-offset="50"
+                data-aos-delay="0"
+                data-aos-duration="1200"
+                data-aos-easing="ease"
+                data-aos-once="true"
+              ></i>
               <div className="text">
                 <h4 className="">Sell your NFT's</h4>
-                <p>
+                <p
+                  data-aos="fade-up"
+                  data-aos-offset="50"
+                  data-aos-delay="150"
+                  data-aos-duration="1500"
+                  data-aos-easing="ease"
+                  data-aos-once="true"
+                >
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem.
                 </p>
